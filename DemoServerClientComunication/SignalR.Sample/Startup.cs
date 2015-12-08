20aa0e5c813fc276e309b12b0b02007a43672995
troppoleo <troppoleo@gmail.com>
2015-12-08 16:10:13 +0100

    CompletoOriginale

---
+++ DemoServerClientComunication/SignalR.Sample/Startup.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Microsoft.AspNet.SignalR.StockTicker.Startup1), "Configuration")]
+
+namespace Microsoft.AspNet.SignalR.StockTicker
+{
+    public static class Startup
+    {
+        public static void ConfigureSignalR(IAppBuilder app)
+        {
+            // For more information on how to configure your application using OWIN startup, visit http://go.microsoft.com/fwlink/?LinkID=316888
+
+             app.MapSignalR();
+        }
+    }
+
+    public class Startup1
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            Microsoft.AspNet.SignalR.StockTicker.Startup.ConfigureSignalR(app);
+        }
+    }
+}
+
